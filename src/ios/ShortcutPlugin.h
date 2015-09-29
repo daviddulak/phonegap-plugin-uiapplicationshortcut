6c1caf0d9dbd0022bc7e235e1074c6b5799a3aa1
David Dulak <daviddulak@me.com>
2015-09-28 22:36:07 -0500

    initial commit

---
+++ src/ios/ShortcutPlugin.h
@@ -0,0 +1,28 @@
+// #import <Foundation/Foundation.h>
+// #import <Cordova/CDV.h>
+// #import <Cordova/CDVPlugin.h>
+
+// @interface ShortcutPlugin : CDVPlugin
+// {
+//     NSString *shortcutType;
+//     NSString *shortcutCallbackId;
+//     NSString *callback;
+    
+//     BOOL ready;
+// }
+
+// @property (nonatomic, copy) NSString *callbackId;
+// @property (nonatomic, copy) NSString *shortcutCallbackId;
+// @property (nonatomic, copy) NSString *callback;
+
+// @property (nonatomic, strong) NSString *shortcutType;
+
+// - (void)init:(CDVInvokedUrlCommand*)command;
+
+// - (void)didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
+// - (void)didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
+
+// - (void)setNotificationMessage:(NSDictionary *)notification;
+// - (void)notificationReceived;
+
+// @end
